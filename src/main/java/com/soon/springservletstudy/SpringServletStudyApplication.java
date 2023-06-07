67bf9256b8917a28e59aa366961af2f24cbc7e6e
soonable@naver.com <soonable@naver.com>
2023-06-07 18:23:54 +0900

    Initial commit

---
+++ src/main/java/com/soon/springservletstudy/SpringServletStudyApplication.java
@@ -0,0 +1,13 @@
+package com.soon.springservletstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServletStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringServletStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/soon/springservletstudy/SpringServletStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soon.springservletstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServletStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
