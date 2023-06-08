327fd3f00336a20d79bc3d1663df94a99c77b821
soonable@naver.com <soonable@naver.com>
2023-06-08 15:23:50 +0900

    feat : HTTP request json

--- src/main/java/com/soon/springservletstudy/basic/request/RequestParamServlet.java
+++ src/main/java/com/soon/springservletstudy/basic/request/RequestParamServlet.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Enumeration;
 
 /**
  * 1. 파라미터 전송 기능
