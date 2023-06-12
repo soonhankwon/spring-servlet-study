23b1167e7b4dd1d8ae1464b741b2f951cd54dbf6
soonable@naver.com <soonable@naver.com>
2023-06-12 17:59:22 +0900

    feat : Model 추가 - V3

--- src/main/java/com/soon/springservletstudy/web/frontcontroller/v3/controller/MemberSaveControllerV3.java
+++ src/main/java/com/soon/springservletstudy/web/frontcontroller/v3/controller/MemberSaveControllerV3.java
@@ -1,6 +1,7 @@
 package com.soon.springservletstudy.web.frontcontroller.v3.controller;
 
 import com.soon.springservletstudy.domain.member.Member;
+import com.soon.springservletstudy.domain.member.MemberRepository;
 import com.soon.springservletstudy.web.frontcontroller.ModelView;
 import com.soon.springservletstudy.web.frontcontroller.v3.ControllerV3;
 
@@ -8,12 +9,15 @@ import java.util.Map;
 
 public class MemberSaveControllerV3 implements ControllerV3 {
 
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
     @Override
     public ModelView process(Map<String, String> paramMap) {
         String username = paramMap.get("username");
         int age = Integer.parseInt(paramMap.get("age"));
 
         Member member = new Member(username, age);
+        memberRepository.save(member);
 
         ModelView mv = new ModelView("save-result");
         mv.getModel().put("member", member);
